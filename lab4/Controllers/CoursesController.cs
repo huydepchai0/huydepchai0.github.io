e83cbd6290489a7e7bc59d7ab885345cf9d688b9
Administrator <Administrator@PC427>
2022-06-03 11:10:23 +0700

    Add project files.

---
+++ lab4/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using lab4.Models;
+using lab4.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab4.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
