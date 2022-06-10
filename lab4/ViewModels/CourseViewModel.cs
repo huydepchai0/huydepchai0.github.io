db896a0ae6136b7a643b14da497c825c2df061d3
huydepchai0 <99706703+huydepchai0@users.noreply.github.com>
2022-06-10 09:18:14 +0700

    Update CourseViewModel.cs

--- lab4/ViewModels/CourseViewModel.cs
+++ lab4/ViewModels/CourseViewModel.cs
@@ -8,9 +8,13 @@ namespace lab4.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDatatime()
