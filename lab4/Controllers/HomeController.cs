1af2cac25ae7b840600dfd44ae9b6c2064d9da1b
huydepchai0 <99706703+huydepchai0@users.noreply.github.com>
2022-06-23 00:39:44 +0700

    Update HomeController.cs

--- lab4/Controllers/HomeController.cs
+++ lab4/Controllers/HomeController.cs
@@ -7,6 +7,19 @@ using System.Web.Mvc;
 namespace lab4.Controllers
 {
     public class HomeController : Controller
+    private ApplicationDbContext _dbContext;
+    public HomeController()
+    {
+        _dbContext = new ApplicationDbContext();
+    }
+    public ActionResult Index()
+    {
+        var upcommingCourses = _dbContext.Courses
+            .Include(c => c.Lecturer)
+            .Include(c => c.Category)
+            .Where(c => c.DateTime > DateTime.Now);
+        return View(upcommingCourses);
+    }   
     {
         public ActionResult Index()
         {
